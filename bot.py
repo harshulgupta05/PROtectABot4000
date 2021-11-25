0ed38deafab9933722455690ad93468f1c0e5136
arnavgupta03 <harshulgupta05@gmail.com>
2021-11-24 21:37:32 -0500

    added server data saving

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import discord, os , dotenv
+import discord, os , dotenv, json
 
 from dotenv import load_dotenv
 
@@ -13,13 +13,46 @@ bot = commands.Bot(command_prefix='PROT')
 async def ready():
     print(f'{bot.user.name} has connected to Discord.')
 
+@bot.event
+async def on_join(guild):
+    path = "servers/" + str(guild.id)
+    try:
+        os.mkdir(path)
+
+        data = {
+            "flags": ["flag"],
+            "members": guild.members,
+        }
+
+        path = path + "/settings.json"
+
+        os.mkdir(path)
+    except FileExistsError:
+        path = "servers/" + str(guild.id) + "/settings.json"
+    
+@bot.command(name='add')
+async def add_to_flags(ctx, arg):
+    if ctx.message.author.guild_permissions.administrator:
+        path = "servers/" + str(ctx.guild.id) + "/settings.json"
+        serverSettings = json.load(open(path, 'r'))
+        serverSettings['flags'] = serverSettings['flags'].append(arg)
+
+        os.remove(path)
+
+        json.dump(serverSettings, open(path, 'w'))
+    else:
+        await ctx.send("Only admins can add to the list of flags.")
+
+
 # deletes messages in list "word"
-@client.event
+@bot.event
 async def on_message(message):
     msg_content = message.content.lower()
-    words = ["chink"]
-    if any(word in msg_content for word in words):
-        await message.delete()
+    path = "servers/" + str(message.guild.name) + "/settings.json"
+    data = json.load(open(path, 'r'))
+    flags = data['flags']
+    if any(flag in msg_content for flag in flags):
+        await message.channel.send("You used a bad word!")
 
 
     
