278338ee277d2b8bb9fecd128c1c2b4d4200ea8d
pjax42 <76502486+pjax42@users.noreply.github.com>
2021-11-26 00:33:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,56 +16,6 @@ suggested_flags = []
 message_url = []
 help_list = []
 
-# client = discord.Client()
-# 
-# @client.event
-# async def on_ready():
-#     print(f'{bot.user.name} has connected to Discord.')
-#     # await channel.send(f'{bot.user.name} has connected to Discord')
-# 
-# @client.event
-# async def on_join():
-#     for guild in bot.guilds:
-#         path = "servers/" + str(guild.id)
-#         try:
-#             os.mkdir(path)
-# 
-#             data = {
-#                 "flags": ["flag"],
-#                 "members": guild.members,
-#             }
-# 
-#             path = path + "/settings.json"
-# 
-#             os.mkdir(path)
-#         except FileExistsError:
-#             path = "servers/" + str(guild.id) + "/settings.json"
-
-# deletes messages in list "word"
-# @bot.event
-# async def on_message(message):
-#     msg_content = message.content.lower()
-#     path = "servers/" + str(message.guild.id) + ".json"
-#     data = json.load(open(path, 'r'))
-#     flags = data['flags']
-#     if any(flag in msg_content for flag in flags):
-#         await message.channel.send("You used a bad word!")
-
-# @bot.event
-# async def on_message_url(message):    
-#     msg_content = message.content.lower()
-#     path = "servers/" + str(message.guild.id) + ".json"
-#     data = json.load(open(path, 'r'))
-#     bad_webs = data['urls']
-#     #if link, check it, reply to message
-#     if "https://" in msg_content:
-#         name = msg_content.split("https://www.")[1].split(".com")[0]
-#         print(name)
-#         for website in bad_webs.keys():
-#             print(str(website))
-#             if str(website).lower() in name.lower():
-#                 await message.reply(f'The url {message.author.name} has posted is for the site {website} which, according to AllSides.com has a bias of {bad_webs.get(website)}. View the linked media at your own discretion.')
-
 @bot.command(name='add')
 async def add_to_flags(ctx, arg):
     if ctx.message.author.guild_permissions.administrator:
@@ -115,7 +65,6 @@ async def suggest(ctx, arg):
 
 @bot.command(name='suggested')
 async def suggested(ctx):
-    await ctx.reply("working")
     path = "servers/" + str(ctx.guild.id) + ".json"
     serverSettings = json.load(open(path, 'r'))
     currentSuggestedFlags = serverSettings['suggestedFlags']
